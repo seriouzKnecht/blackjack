203b4c8416b0cf918dad1ae837854ea0aef9837a
Mirko Greese <mirko.greese@campus.tu-berlin.de>
2015-05-23 22:40:42 +0200

    -viewport+cam init

---
+++ core/src/knecht/game/Assets.java
@@ -0,0 +1,11 @@
+package knecht.game;
+
+import com.badlogic.gdx.assets.AssetLoaderParameters;
+import com.badlogic.gdx.assets.loaders.AssetLoader;
+
+public class Assets {
+
+	public void load() {
+	
+	}
+}
--- core/src/knecht/game/Blackjack.java
+++ core/src/knecht/game/Blackjack.java
@@ -3,25 +3,63 @@ package knecht.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class Blackjack extends ApplicationAdapter {
+	
+	int GAME_WIDTH = 1280;
+	int GAME_HEIGHT = 720;
+	
 	SpriteBatch batch;
-	Texture img;
+	Sprite img;
+	private OrthographicCamera cam;
+	private StretchViewport viewport;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Sprite(new Texture("badlogic.jpg"));
+		img.setPosition(0, 360);
+		initCam();
+	}
+	
+	public void resize(int width, int height) {
+	    viewport.update(width, height);
+	}
+
+	private void initCam() {
+		cam = new OrthographicCamera(GAME_WIDTH, GAME_HEIGHT);
+		cam.setToOrtho(false, GAME_WIDTH, GAME_HEIGHT);
+		viewport = new StretchViewport(GAME_WIDTH, GAME_HEIGHT, cam);
+		batch.setProjectionMatrix(cam.combined);
 	}
 
 	@Override
 	public void render () {
+		
+		update(Gdx.graphics.getDeltaTime());
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(cam.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, img.getX(), img.getY());
 		batch.end();
+	}
+
+	private void update(float deltaTime) {
+		img.setX((img.getX() + 128 * deltaTime) % 1280);
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		batch.dispose();
+		img.getTexture().dispose();
 	}
 }
